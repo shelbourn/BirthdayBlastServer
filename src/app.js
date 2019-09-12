74e69226f2a029f01fda5945363e067b2a163539
shelbourn <mattshelbourn@gmail.com>
2019-09-12 16:30:12 -0700

    Changing app.js for updated routes. /math route.
    Co-authored-by: Nick Anderson <nickanderson@gmail.com>

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ require('dotenv').config()
 
 const middlewares = require('./middlewares')
 const api = require('./api')
+const api2 = require('./api/api2')
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(helmet())
-console.log(process.env.test.NODE_ENV)
+console.log('ENV: ', process.env.NODE_ENV)
 app.get('/', (req, res) => {
 	res.json({
 		message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
@@ -19,10 +20,11 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-	console.log(req)
+	console.log(req.body)
 })
 
 app.use('/api/v1', api) // Establises 'v1' as route
+app.use('/api2', api2)
 
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
