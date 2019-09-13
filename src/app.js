a02ff8071456f027ca7d5c7a88ab61578973255f
shelbourn <mattshelbourn@gmail.com>
2019-09-12 17:07:02 -0700

    Adding code for server posting.

--- src/app.js
+++ src/app.js
@@ -10,17 +10,27 @@ const api2 = require('./api/api2')
 
 const app = express()
 
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json())
 app.use(morgan('dev'))
 app.use(helmet())
 console.log('ENV: ', process.env.NODE_ENV)
+
 app.get('/', (req, res) => {
 	res.json({
 		message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
 	})
 })
 
-app.post('/', (req, res) => {
+app.post('/post', (req, res) => {
 	console.log(req.body)
+	// const value = req.body.data
+	// if (typeof value === 'number') {
+	// 	res.json({ message: value + 1000 })
+	// } else {
+	// 	res.json({ anotherMessage: 'send a number FOo! 🦄' })
+	// }
+	res.json(req.body)
 })
 
 app.use('/api/v1', api) // Establises 'v1' as route
