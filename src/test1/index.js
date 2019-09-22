be0c14c7d1015b508f1a8f2822c01c9bf24c789d
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-09-21 20:27:44 -0700

    Updates.

--- src/test1/index.js
+++ src/test1/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 
-const math = require('./math')
+const randomString = require('./randomString')
 
 const router = express.Router()
 
-router.use('/math', math)
+router.use('/randomString', randomString)
 
 module.exports = router
