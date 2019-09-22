fdc9bab9ac7a959943a8fb004a9bf689c4b39656
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-09-21 21:24:37 -0700

    Calling it quits for the night.

--- src/test1/randomString.js
+++ src/test1/randomString.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-	res.json([Math.random()])
+	res.json([String.random()])
 })
 
 module.exports = router
