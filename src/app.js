94fc48c2b2cfb15f44a299175f5aece2d569784e
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-09-21 20:35:35 -0700

    Testing.

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const middlewares = require('./middlewares')
 const api = require('./api')
 const api2 = require('./api/api2')
+const randomString = require('./test1')
 
 const app = express()
 
@@ -35,6 +36,7 @@ app.post('/post', (req, res) => {
 
 app.use('/api/v1', api) // Establises 'v1' as route
 app.use('/api2', api2)
+app.use('/randomString', randomString)
 
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
