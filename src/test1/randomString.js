6fd9ab917f4b010b3fa1a4b9c858768120fea310
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-09-21 21:20:51 -0700

    Route working for get.

--- src/test1/randomString.js
+++ src/test1/randomString.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-	res.json([math.random()])
+	res.json([Math.random()])
 })
 
 module.exports = router
