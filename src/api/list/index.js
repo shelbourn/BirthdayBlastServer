296ec298a5aadb1a7779f9a6fd1f343087f0f5f6
shelbourn <mattshelbourn@gmail.com>
2019-09-23 14:19:03 -0700

    Adding new routes and files for list and admin.

---
+++ src/api/list/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const emojis = require('./emojis')
+const newRoute = require('./newRoute')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+	res.json({
+		message: 'API - 👋🌎🌍🌏'
+	})
+})
+
+router.use('/emojis', emojis)
+router.use('/newRoute', newRoute) // Establishes 'newRoute' as new route.
+
+module.exports = router
