164f58063a61f9cff9054373251c6cf9f252da61
shelbourn <mattshelbourn@gmail.com>
2019-09-23 14:36:05 -0700

    Establishing new routes.

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -1,17 +1,9 @@
 const express = require('express')
 
-const emojis = require('./emojis')
-const newRoute = require('./newRoute')
+const adminGet = require('./get')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-	res.json({
-		message: 'API - 👋🌎🌍🌏'
-	})
-})
-
-router.use('/emojis', emojis)
-router.use('/newRoute', newRoute) // Establishes 'newRoute' as new route.
+router.use('/adminGet', adminGet)
 
 module.exports = router
