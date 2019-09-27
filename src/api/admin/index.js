e04b70644427ca6e6335e164a328515fa2efd86d
shelbourn <mattshelbourn@gmail.com>
2019-09-27 14:46:12 -0700

    Adding routes for put and delete.

--- src/api/admin/index.js
+++ src/api/admin/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const adminGet = require('./get')
 const adminPost = require('./post')
+// const adminDelete = require('./delete')
+// const adminPut = require('./put')
 
 const router = express.Router()
 
 router.use('/adminGet', adminGet)
 router.use('/adminPost', adminPost)
+// router.use('/adminDelete', adminDelete)
+// router.use('./adminPut', adminPut)
 
 module.exports = router
--- src/api/list/index.js
+++ src/api/list/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const listGet = require('./get')
 const listPost = require('./post')
+// const listDelete = require('./delete')
+// const listPut = require('./put')
 
 const router = express.Router()
 
 router.use('/listGet', listGet)
 router.use('/listPost', listPost)
+// router.use('/listDelete', listDelete)
+// router.use('/listPut', listPut)
 
 module.exports = router
