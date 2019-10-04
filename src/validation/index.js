5f69e40188365b0f1c8f3005b28213654bcd877a
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-03 18:48:06 -0700

    Testing new validation. `Or` operator. :rocket:

--- src/validation/index.js
+++ src/validation/index.js
@@ -8,7 +8,7 @@ const validationObjectSize = body => {
 }
 
 const validatePhone = body => {
-	if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(body.phone)) {
+	if (/^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/.test(body.phone)) {
 		return true
 	}
 	return false
@@ -17,20 +17,20 @@ const validatePhone = body => {
 const validateParams = body => {
 	// const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
 	if (
-		typeof body.name !== 'string' &&
-		typeof body.email !== 'string' &&
-		typeof body.phone !== 'string' &&
-		typeof body.bday !== 'string' &&
-		typeof body.notificationTime !== 'string' &&
-		validatePhone === true
+		typeof body.name !== 'string' ||
+		typeof body.email !== 'string' ||
+		typeof body.phone !== 'string' ||
+		typeof body.bday !== 'string' ||
+		typeof body.notificationTime !== 'string' ||
+		validatePhone !== true
 	) {
-		return body
+		return false
 		// if (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(body.phone)) {
 		// 	return body.phone
 		// }
 		// return false
 	}
-	return false
+	return true
 }
 
 module.exports = {
