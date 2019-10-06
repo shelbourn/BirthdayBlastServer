7f307935061878e75bf7b96996f1852ebd073e46
Matthew Shelbourn <mattshelbourn@gmail.com>
2019-10-06 13:37:53 -0700

    Code not working completely.

--- src/api/list/listPut.js
+++ src/api/list/listPut.js
@@ -39,9 +39,6 @@ router.put('/', (req, res) => {
 			listDataStore[i] = req.body // Pushed data to record based on email as PK
 			// listDataStore.splice(i) ->> delete data
 		}
-		const error = res.status(400).send('Oops, try again!') // is this correct?
-		console.log(error)
-		// return error
 		return data
 		// const error = res.status(400).send('Oops, try again!') // is this correct?
 		// console.log(error)
