ba5635a250adb4b1a58d97e754566c056bfec078
shelbourn <mattshelbourn@gmail.com>
2019-10-08 15:13:01 -0700

    Modifying List index.js file.
    Co-authored-by: Nick Anderson <nickanderson@gmail.com>

--- src/data/index.js
+++ src/data/index.js
@@ -3,23 +3,39 @@
 const data = {
 	adminData: ['🦄', '🌈', '👋'],
 	listData: ['😀', '😳', '🙄'],
+
+	// ListBdayTable
 	listDataStore: [
 		{
-			name: 'Erica',
+			foreignKey: 1,
+			name: 'Andy',
 			email: 'a@aol.com',
 			phone: '562',
 			bday: '9-27',
 			notificationTime: '8am'
 		},
 		{
-			name: 'Bob',
+			foreignKey: 1,
+			firstName: 'Mike',
+			lastName: 'Shelbourn',
 			email: 'b@aol.com',
 			phone: '562',
 			bday: '9-27',
 			notificationTime: '8am'
 		}
 	],
-	adminDataStore: []
+	// AdminTable
+	adminDataStore: [
+		{
+			primaryKey: 1,
+			firstName: 'Matt',
+			lastName: 'Shelbourn',
+			email: 'admin@aol.com',
+			phone: '562',
+			bday: '9-27',
+			notificationTime: '8am'
+		}
+	]
 }
 
 // Spread operator seperates data into its separate object
