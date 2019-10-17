c35dd61b15de9ae40e4b6201ab9be4d0ed0cc1b1
shelbourn <mattshelbourn@gmail.com>
2019-10-17 16:55:54 -0700

    Adding SQL files.

---
+++ src/mySql/admin.sql
@@ -0,0 +1,4 @@
+/*SELECT *
+FROM sys.admin */
+INSERT INTO sys.admin (firstname, lastname, email, phone, notificationtime)
+VALUES ('Matthew', 'Shelbourn', 'mattshelbourn@gmail.com', '213-316-8766', '10 am');
